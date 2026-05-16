1a2fddcda6c49d06f7fe262a9136abd3ce2cfe8c
vivek <vivekchiguru8@gmail.com>
2026-05-16 17:04:00 +0530

    k

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "terraform-bucket"
-  location      = "US"
-  force_destroy = true
+  name                     = "terraform-bucket"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
 
