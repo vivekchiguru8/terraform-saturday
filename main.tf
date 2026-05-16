c9b4cdfab226fd50fd6b110159037294a9fdf082
vivek <vivekchiguru8@gmail.com>
2026-05-16 17:32:25 +0530

    initial code

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "terraform-bucket"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+  name                        = "terraform-bucket-project-8f3b3c8e-4647-4878-8b5"
+  project                     = "project-8f3b3c8e-4647-4878-8b5"
+  location                    = "US"
+  force_destroy               = true
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
 }
-
